93541d26cee50efa89ea9a1fc1d969fc325b3309
SushobhanLimbole <sushobhanlimbole17@gmail.com>
2025-06-20 12:30:06 +0530

    video test

--- src/socket.js
+++ src/socket.js
@@ -3,8 +3,8 @@ import { io } from "socket.io-client";
 const socket = io("https://pairpro-backend-backup.onrender.com", {
   transports: ["websocket"],
   autoConnect: true, // Auto connect once globally
-  reconnectionAttempts: 5,
-  timeout: 10000,
+  // reconnectionAttempts: 5,
+  // timeout: 10000,
 });
 
 export { socket };
